900cd78754e7a31720d8d99cb922c685d2572188
Olivier-St-Amand79 <blackburnerik061@gmail.com>
2025-02-12 14:51:36 -0500

    S4C1D1

--- Assets/Scripts/MouvementTranslateWorld.cs
+++ Assets/Scripts/MouvementTranslateWorld.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public class MouvementTranslateWorld : MonoBehaviour
 {
+    [SerializeReference] float _vitesse_x = 0f;
+    [SerializeReference] float _vitesse_z = 0f;
+    [SerializeReference] float _vitesse_y = 0f;
     void Update()
     {
-        transform.Translate(1f * Time.deltaTime, 0, 0, Space.World);
+        transform.Translate(_vitesse_x * Time.deltaTime, _vitesse_y * Time.deltaTime, _vitesse_z * Time.deltaTime, Space.World);
 
         //Vector3 deplacement = new Vector3(1f, 0, 0);
         //transform.Translate(deplacement * Time.deltaTime, Space.World);
